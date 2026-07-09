3bd37bc64c49293a1dc5fd60b3aeecc510b227f5
GadgetHub123 <tjayyy78@gmail.com>
2026-07-09 22:47:23 +0800

    Update to permanent ngrok URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { io } from "socket.io-client";
 
-const API_URL = "https://covered-foo-immediate-ash.trycloudflare.com";
+const API_URL = "https://absurd-embattled-uncounted.ngrok-free.dev";
 
 export const api = axios.create({
   baseURL: API_URL,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 export const socket = io(API_URL, {
